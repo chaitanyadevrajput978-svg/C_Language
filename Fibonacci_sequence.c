d36907a8918252d382a3c54108624aee560523a0
Chaitanya Dev Rajput <chaitanyadevrajput978@gmail.com>
2025-11-08 17:32:21 +0530

    Add files via upload

---
+++ Fibonacci_sequence.c
@@ -0,0 +1,17 @@
+// Practical - 15
+
+#include<stdio.h>
+int main(){
+    int n,a=-1,b=1,c;
+    
+    printf("enter a number: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+return 0;
+}
