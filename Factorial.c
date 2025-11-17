78a6bd462bc559332ee4f03ec421a048cb7052b3
Chaitanya Dev Rajput <chaitanyadevrajput978@gmail.com>
2025-11-17 11:16:51 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    double fac=1;
+    printf("enter a number: ");
+    scanf("%d",&n);
+
+    if(n!=0){
+        for(int i=1;i<=n;i++){
+        fac = fac*i;
+        }
+    }
+    else{
+        printf("factorial=1");
+    
+    }
+    printf("factorial of %d is %lf",n,fac);
+    return 0;
+
+}
